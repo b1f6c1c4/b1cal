e8b385b43d0a20bfff6f8c91936ed01689753aaa
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-04-20 23:11:58 -0400

    art

---
+++ build.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+
+const Bundler = require('parcel-bundler');
+const netlifyLambda = require('netlify-lambda/lib/build');
+const shell = require('shelljs');
+
+process.env.NODE_ENV = 'production';
+
+async function makeBundle() {
+  const bundler = new Bundler([
+    './index.html',
+    './app.html',
+  ]);
+
+  await bundler.bundle();
+  const html = shell.cat('art/html_code.html');
+  shell.sed('-i', /<title>/, `${html}<title>`, 'dist/*.html');
+  shell.cp('art/*', 'dist/');
+}
+
+async function buildFunctions() {
+  const stats = await netlifyLambda.run('functions', undefined);
+  console.log(stats.toString({ color: true }));
+}
+
+makeBundle();
+buildFunctions();
--- ci-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-npm run build
-npm run build:functions
